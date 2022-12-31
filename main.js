020df688ebe0ae0d180d42faa7b4eb32eb452da0
Hero-Gamer727 <83517823+Hero-Gamer727@users.noreply.github.com>
2022-12-31 12:07:01 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 status_model = "";
 video = "";
-
+objects = []
 
     
 
@@ -25,5 +25,27 @@ function modelLoaded(){
 
 function draw(){
     image(video,0,0,480,380)
+    if (status_model != ""){
+        objectDetector.detect(video, gotResults);
+        for (i=0;i< objects.length; i++){
+            document.getElementById("status").innerHTML="Status: Objects Detected";
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + ""+ percent+"%",objects[i].x + 15, objects[i].y+ 15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    
+    }
 
+}
+
+function gotResults(error, results){
+    if (error){
+        console.error(error)
+    } else{
+        console.log(results)
+        objects = results
+    }
 }
